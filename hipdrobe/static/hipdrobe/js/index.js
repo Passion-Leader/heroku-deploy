b5b8c0c490ce8dae470c3d31a7b7a59a53259747
codeorchord <codeorchord@gmail.com>
2019-12-12 18:55:00 +0900

    init

---
+++ hipdrobe/static/hipdrobe/js/index.js
@@ -0,0 +1,22 @@
+/*
+ *  index.js
+ */
+
+
+$(document).ready(function(){
+    // Index page는 로고 흰색
+    changeLogColor('white')
+
+    // Index page는 페이지 제목 없음
+    setTitle("");
+
+    // Carousel 밑의 컨텐츠 시작 위치 맞추기
+    fitBottom();
+
+});
+
+window.onresize = function(event) {
+    fitBottom();
+};
+
+
